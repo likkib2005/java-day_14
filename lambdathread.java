a52622b41ffc3ad219e5e044100f2b97d6df0430
likitha <likkilikitha214@gmail.com>
2024-12-15 11:29:09 +0530

    Add files via upload

---
+++ lambdathread.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String args[])
+    {
+        Runnable r=()->System.out.println("I am new from java version");
+        Thread t=new Thread(r);
+        t.start();
+        System.out.println(t.isAlive());
+    }
+}
