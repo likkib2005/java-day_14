7922047eb20aa61dbe6f76846963c1c3a1179baf
likitha <likkilikitha214@gmail.com>
2024-12-15 12:26:41 +0530

    Add files via upload

---
+++ threadsleep.java
@@ -0,0 +1,11 @@
+class lifecyclethread {
+    public static void main(String[] args) {
+        System.out.println("Thread started");
+        try {
+            Thread.sleep(5000);  
+        } catch (InterruptedException e) {  
+            System.out.println("Interrupted");  
+        }
+        System.out.println("Thread resumed");
+    }
+}
