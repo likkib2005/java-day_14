f28ef326c9cea49a3dc6d179471362976225b809
likitha <likkilikitha214@gmail.com>
2024-12-15 11:21:10 +0530

    Add files via upload

---
+++ extendthread.java
@@ -0,0 +1,17 @@
+
+class a extends Thread
+{
+    public void r()
+    {
+        System.out.println("Hello I am Thread");
+    }
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        a g=new a();
+        g.r();
+        g.start();
+    }
+}
